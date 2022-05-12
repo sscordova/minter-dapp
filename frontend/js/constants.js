cc9c6a1a4764aec371c44d1745f7fc39f0cc59ec
sscordova <sscordova>
2022-05-12 14:04:05 -0300

    cambios

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x3527B8464C036155B4fbeA07edD03d607aB57CA1"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
